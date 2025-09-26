b82aedc16d8df4e50e62605a52c348012b10fba3
codemonk-silver <olajugbagbeeniola12@outlook.com>
2025-09-26 02:45:51 -0700

    account form uopdate

--- e-commerce/src/component/Account/account.js
+++ e-commerce/src/component/Account/account.js
@@ -4,9 +4,27 @@ import './account.css';
 const Account = () => {
   return (
     <section className='accountContainer'>
-        
+      <div className='accountWrapper'>
+        <div className='accountNav'>
+          <a href=''>
+            <span>Home</span>
+            <span className="arrow">{'>'}</span>
+          </a>
+          <a href=''>
+            <span>Account</span>
+            <span className="arrow">{'>'}</span>
+          </a>
+          <a href=''>Sign In</a>
+        </div>
+
+        <p>Sign In</p>
+      </div>
+
+      <div className='accountForm'>
+            
+      </div>
     </section>
   )
 }
 
-export default Account
+export default Account;
